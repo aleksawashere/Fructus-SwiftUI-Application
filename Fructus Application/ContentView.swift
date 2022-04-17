fd10f4d2c347111a1ea408e4bf24d4ec128a017a
aleksawashere <aleksa.dimitrijevic.business@gmail.com>
2022-04-17 23:18:43 +0200

    Initial Commit

---
+++ Fructus Application/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fructus Application
+//
+//  Created by Aleksa Dimitrijevic on 17.4.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fructus Application/Fructus_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fructus_ApplicationApp.swift
+//  Fructus Application
+//
+//  Created by Aleksa Dimitrijevic on 17.4.22..
+//
+
+import SwiftUI
+
+@main
+struct Fructus_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
